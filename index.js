7c03b671c01f269b2eabe17614ea4166867fd26c
phani <pasupulaphani@gmail.com>
2018-12-13 19:51:30 +0100

    added err handling

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const iptablesService = require('./services/iptables')
 const app = express()
 const port = 3000
 
+// error handler
+app.use(function (err, req, res, next) {
+  function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+      return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+  }
+})
+
 app.get('/bytes-networks', async (req, res) => {
   const bytesAps = await wifiService.getNetworks();
   res.send(bytesAps)
